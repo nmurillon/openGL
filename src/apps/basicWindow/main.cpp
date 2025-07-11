10c4dae5651a5f6ef63221efbe9a29fd02c9b6f5
nmurillon <nmurillon@gmail.com>
2025-07-11 14:49:50 +0200

    [basicWindow] draw first triangle

--- src/apps/basicWindow/main.cpp
+++ src/apps/basicWindow/main.cpp
@@ -2,6 +2,22 @@
 
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <vector>
+
+const char *vertexShaderSrc =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSrc = "#version 330 core\n"
+                                "out vec4 FragColor;\n"
+                                "void main()\n"
+                                "{\n"
+                                "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                "}\0";
 
 // Callback to resize the window if user resizes it
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
@@ -34,6 +50,52 @@ int main() {
     return -1;
   }
 
+  // SETUP SHADERS
+  const uint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSrc, NULL);
+  glCompileShader(vertexShader);
+
+  const uint fragementShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragementShader, 1, &fragmentShaderSrc, NULL);
+  glCompileShader(fragementShader);
+
+  const uint shaderProgram = glCreateProgram();
+
+  // Beware to the order
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragementShader);
+
+  glLinkProgram(shaderProgram);
+
+  // Once linked, shaders can be deleted
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragementShader);
+
+  // SETUP VERTEX DATA
+  std::vector<double> vertices{
+      -0.5f, -0.5f, 0.f, 0.f, 0.5f, 0.f, 0.5f, -0.5f, 0.f,
+  };
+
+  // VAO & VBO
+  uint VAO, VBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(double),
+               vertices.data(), GL_STATIC_DRAW);
+
+  // Link Vertex attributes
+  glVertexAttribPointer(0, 3, GL_DOUBLE, GL_FALSE, 3 * sizeof(double),
+                        (void *)0);
+  glEnableVertexAttribArray(0);
+
+  // Unbind VBO & VAO to prevent further operations to modify them
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBindVertexArray(0);
+
   glViewport(0, 0, 800, 600);
 
   // Wait for user input to keep the window opened
@@ -42,6 +104,10 @@ int main() {
 
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
