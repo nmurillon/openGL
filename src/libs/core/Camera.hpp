1b486b7cac766931defe708aca92e664d1ad9283
nmurillon <nmurillon@gmail.com>
2025-09-17 16:20:56 +0200

    [core] add Camera class

---
+++ src/libs/core/Camera.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace libs::core {
+
+enum class CameraMovement {
+  FORWARD,
+  BACKWARD,
+  LEFT,
+  RIGHT,
+};
+
+class Camera {
+public:
+  Camera(const glm::vec3 &position = glm::vec3(0.0f, 0.0f, 0.0f),
+         const glm::vec3 &front = glm::vec3(0.0f, 0.0f, -1.0f),
+         const glm::vec3 &up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = -90.f,
+         float pitch = 0.f);
+
+  glm::mat4 getViewMatrix() const;
+  void processMouseMovement(float xOffset, float yOffset,
+                            GLboolean constraintPitch = true);
+  void processKeyboardInput(const CameraMovement &direction, float deltaTime);
+  void processMouseScroll(float yOffset);
+
+private:
+  void update();
+
+  glm::vec3 m_position;
+  glm::vec3 m_front;
+  glm::vec3 m_up;
+  glm::vec3 m_right;
+
+  glm::vec3 m_worldUp;
+
+  float m_yaw;
+  float m_pitch;
+
+  float m_movementSpeed{2.5f};
+  float m_mouseSensitivity{0.1f};
+  float m_zoom{45.f};
+};
+} // namespace libs::core
