3d1dbab1546ae757ff4363c3cdc602986e335003
nmurillon <nmurillon@gmail.com>
2025-10-24 11:47:47 +0200

    [3rdParty] add imgui

--- src/libs/core/Window.cpp
+++ src/libs/core/Window.cpp
@@ -1,5 +1,9 @@
 #include <libs/core/Window.hpp>
 
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+
 #include <stdexcept>
 
 namespace libs::core {
@@ -24,18 +28,42 @@ Window::Window(int width, int height, std::string title) {
   if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
     throw std::runtime_error("Failed to initialize GLAD");
   }
+
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO &io = ImGui::GetIO();
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+  // Setup Platform/Renderer backends
+  ImGui_ImplGlfw_InitForOpenGL(m_window, true);
+  ImGui_ImplOpenGL3_Init();
 }
 
-Window::~Window() { glfwTerminate(); }
+Window::~Window() {
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+  glfwTerminate();
+}
 
 void Window::open(std::function<void(void)> frameHandler) {
   // Wait for user input to keep the window opened
   while (!glfwWindowShouldClose(m_window)) {
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+    ImGui::ShowDemoWindow();
+
     if (glfwGetKey(m_window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
       glfwSetWindowShouldClose(m_window, true);
     }
     /// TODO: register input handler processInput(window);
     frameHandler();
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
     glfwSwapBuffers(m_window);
     glfwPollEvents();
