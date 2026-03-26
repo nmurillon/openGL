8742e1b5ebaa8b586f13a4e5563bb8f6920e1b00
nmurillon <nmurillon@gmail.com>
2026-03-26 16:10:12 +0100

    [renderer] use buffers in cube

--- src/libs/renderer/Cube.cpp
+++ src/libs/renderer/Cube.cpp
@@ -2,89 +2,36 @@
 
 #include <libs/openGl/opengl.h>
 
-#include <numeric>
+#include <libs/renderer/IndexBuffer.hpp>
+#include <libs/renderer/VertexBuffer.hpp>
 
 namespace libs::renderer {
-Cube::Cube(const DataType &type, const std::vector<float> &vertices,
+Cube::Cube(BufferLayout &&layout, const std::vector<float> &vertices,
            const std::vector<unsigned int> &indices)
-    : m_type(type), m_vertices(vertices), m_indices(indices) {
+    : m_vertices(vertices), m_indices(indices) {
 
-  glGenVertexArrays(1, &m_VAO);
-  glGenBuffers(1, &m_VBO);
-  glBindVertexArray(m_VAO);
-  glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-  glBufferData(GL_ARRAY_BUFFER, m_vertices.size() * sizeof(float),
-               m_vertices.data(), GL_STATIC_DRAW);
+  VertexBuffer vertexBuffer{std::move(layout), vertices.data(),
+                            vertices.size() * sizeof(float)};
 
-  if (!m_indices.empty()) {
-    glGenBuffers(1, &m_EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-                 m_indices.size() * sizeof(unsigned int), m_indices.data(),
-                 GL_STATIC_DRAW);
+  m_vertexArray.setVertexBuffer(std::forward<VertexBuffer>(vertexBuffer));
+
+  if (m_indices.size()) {
+    IndexBuffer indexBuffer{m_indices.data(),
+                            m_indices.size() * sizeof(unsigned int)};
+    m_vertexArray.setIndexBuffer(std::forward<IndexBuffer>(indexBuffer));
   }
-
-  const auto stride = getStride();
-
-  const auto sizes = getAttributeSizes();
-  glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, sizes[0], GL_FLOAT, GL_FALSE, stride,
-                        reinterpret_cast<void *>(0));
-
-  // Vertex attribute 1
-  if (sizes.size() > 1) {
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, sizes[1], GL_FLOAT, GL_FALSE, stride,
-                          reinterpret_cast<void *>(sizes[0] * sizeof(float)));
-  }
-
-  // Vertex attribute 2
-  if (sizes.size() > 2) {
-    glEnableVertexAttribArray(2);
-    glVertexAttribPointer(
-        2, sizes[2], GL_FLOAT, GL_FALSE, stride,
-        reinterpret_cast<void *>((sizes[1] + sizes[0]) * sizeof(float)));
-  }
-
-  // Unbind VBO & VAO to prevent further operations to modify them
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-  glBindVertexArray(0);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 void Cube::draw() const {
-  glBindVertexArray(m_VAO);
+  m_vertexArray.bind();
   if (!m_indices.empty()) {
     glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(m_indices.size()),
                    GL_UNSIGNED_INT, 0);
   } else {
-    glDrawArrays(GL_TRIANGLES, 0,
-                 static_cast<GLsizei>(m_vertices.size() / getVertexOffset()));
+    const auto count = m_vertexArray.getVertexBuffer().getVertexCount();
+    glDrawArrays(GL_TRIANGLES, 0, static_cast<GLsizei>(count));
   }
-}
-
-int Cube::getVertexOffset() const {
-  const auto sizes = getAttributeSizes();
-  return std::accumulate(sizes.begin(), sizes.end(), 0);
-}
-
-int Cube::getStride() const { return getVertexOffset() * sizeof(float); }
-
-std::vector<int> Cube::getAttributeSizes() const {
-  switch (m_type) {
-  case DataType::POSITION:
-    return {3};
-  case DataType::POSITION_COLOR:
-    return {3, 3};
-  case DataType::POSITION_TEXTURE:
-    return {3, 2};
-  case DataType::POSITION2D_TEXTURE:
-    return {2, 2};
-  case DataType::POSITION_NORMAL:
-    return {3, 3};
-  case DataType::POSITION_NORMAL_TEXTURE:
-    return {3, 3, 2};
-  }
+  m_vertexArray.unbind();
 }
 
 }; // namespace libs::renderer
--- src/libs/renderer/Cube.hpp
+++ src/libs/renderer/Cube.hpp
@@ -2,37 +2,23 @@
 
 #include <logl/renderer/export.h>
 
+#include <libs/renderer/BufferLayout.hpp>
+#include <libs/renderer/VertexArray.hpp>
+
 #include <vector>
 
 namespace libs::renderer {
 class LOGL_RENDERER_EXPORT Cube {
 public:
-  enum class LOGL_RENDERER_EXPORT DataType {
-    POSITION,
-    POSITION_COLOR,
-    POSITION2D_TEXTURE,
-    POSITION_TEXTURE,
-    POSITION_NORMAL,
-    POSITION_NORMAL_TEXTURE,
-  };
-
-  Cube(const DataType &type, const std::vector<float> &vertices,
+  Cube(BufferLayout &&layout, const std::vector<float> &vertices,
        const std::vector<unsigned int> &indices = {});
 
   void draw() const;
 
 private:
-  int getVertexOffset() const;
-  int getStride() const;
-  std::vector<int> getAttributeSizes() const;
+  VertexArray m_vertexArray;
 
-private:
-  DataType m_type;
-  std::vector<float> m_vertices;
-  std::vector<unsigned int> m_indices;
-
-  unsigned int m_VAO;
-  unsigned int m_VBO;
-  unsigned int m_EBO;
+  std::vector<float> m_vertices{};
+  std::vector<unsigned int> m_indices{};
 };
 } // namespace libs::renderer
